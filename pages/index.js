82d35c1f83275e6064979a234b377ef39db7743a
Mauro <mauro8a_10@hotmail.com>
2020-12-09 20:08:29 -0600

    auth token

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import Footer from '../components/Footer'
 import CallToAction from '../components/CallToAction'
 import ExperienceCard from '../components/ExperienceCard'
 
+
 // Load Source Sans Pro typeface
 // require('typeface-source-sans-pro')
 
@@ -45,11 +46,10 @@ export default function Home() {
       }}>
         <h1 className=" self-end mb-4 col-span-1 md:col-span-12  text-white text-center text-5xl font-extralight">Experiencias increíbles,<br />contadas por personas únicas</h1>
           <form className="col-span-1 md:col-span-9 md:col-start-3 row-start-2 ">
-          <input class="w-full text-center h-12 px-4 mb-2 text-lg text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" type="text" placeholder="Buscar por Ciudad" onChange={e => setSearch(e.target.value)}/>
+          <input className="w-full text-center h-12 px-4 mb-2 text-lg text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" type="text" placeholder="Buscar por Ciudad" onChange={e => setSearch(e.target.value)}/>
 
           </form>
       </section>
-
 
 
         <section className="container mx-auto grid grid-cols-1 md:grid-cols-9 gap-4 p-10 md:p-32">
